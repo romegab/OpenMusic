1c3af09d063effd15f2ec932b7dc9d931ade1be6
romegab <idstoilov@gmail.com>
2018-09-11 17:35:48 +0300

    first commit

---
+++ src/main/java/com/music/demo/model/Question.java
@@ -0,0 +1,38 @@
+package com.music.demo.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "questions")
+public class Question extends AuditModel {
+    @Id
+    @GeneratedValue(generator = "question_generator")
+    @SequenceGenerator(
+            name = "question_generator",
+            sequenceName = "question_sequence",
+            initialValue = 1000
+    )
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String title;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    public void setTitle(String title){
+        this.title=title;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public void setDescription(String description){
+        this.description=description;
+    }
+    public String getDescription(){
+        return this.description;
+    }
+}
